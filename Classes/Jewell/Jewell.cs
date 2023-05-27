9ba7fe09062b63366bbd98fed46bf224caf59981
Eduardo Franco <franc0edu24@gmail.com>
2023-05-27 13:16:46 -0300

    feat: started project

---
+++ Classes/Jewell/Jewell.cs
@@ -0,0 +1,18 @@
+﻿namespace JewellNS;
+public class Jewell
+{
+    public Jewell(String color, int point, string name)
+    {
+        this.Color = color;
+        this.Point = point;
+        this.Name = name;
+    }
+    public string Color { get; set; }
+    public string Name { get; set; }
+    public int Point { get; set; }
+
+    public string toString()
+    {
+        return $"Cor: {this.Color}, Point: {this.Point}, Name: {this.Name}";
+    }
+}
---
+++ JewellCollector.cs
@@ -0,0 +1,41 @@
+﻿namespace JewellNS
+{
+    public class JewelCollector
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Para sair do programa digite 'quit' ");
+            bool running = true;
+            do
+            {
+                Console.WriteLine("Enter the command: ");
+                string command = Console.ReadLine()!;
+
+                if (command.Equals("quit"))
+                {
+                    running = false;
+                }
+                else if (command.Equals("w"))
+                {
+                    Console.WriteLine("w");
+                }
+                else if (command.Equals("a"))
+                {
+                    Console.WriteLine("a");
+                }
+                else if (command.Equals("s"))
+                {
+                    Console.WriteLine("s");
+                }
+                else if (command.Equals("d"))
+                {
+                    Console.WriteLine("d");
+                }
+                else if (command.Equals("g"))
+                {
+                    Console.WriteLine("g");
+                }
+            } while (running);
+        }
+    }
+}
